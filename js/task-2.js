9da38eb983ced3fbed4018ee0108c575d5ada862
ArtemNozhenko <artem2516nozh@gmail.com>
2024-02-24 16:52:05 +0100

    add

--- js/task-2.js
+++ js/task-2.js
@@ -1,40 +1,26 @@
 const images = [
   {
-    url: '<https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?dpr=2&h=750&w=1260>',
+    url: "https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?dpr=2&h=750&w=1260",
     alt: 'White and Black Long Fur Cat',
   },
   {
-    url: '<https://images.pexels.com/photos/213399/pexels-photo-213399.jpeg?dpr=2&h=750&w=1260>',
+    url: 'https://images.pexels.com/photos/213399/pexels-photo-213399.jpeg?dpr=2&h=750&w=1260',
     alt: 'Orange and White Koi Fish Near Yellow Koi Fish',
   },
   {
-    url: '<https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?dpr=2&h=750&w=1260>',
+    url: 'https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?dpr=2&h=750&w=1260',
     alt: 'Group of Horses Running',
   },
 ];
 
-// const galleryEl = document.querySelector('.gallery');
+const galleryEl = document.querySelector('.gallery');
 
-// const itemImageEl = images.map(image => {
-//   const imageEl = document.createElement('img');
-//   imageEl.url = image.url;
-//   imageEl.alt = image.alt;
-//   imageEl.width = 360;
-//   imageEl.height = 300;
-//   return itemImageEl;
-// });
-// // const itemEl = document.createElement('li');
-// // itemImageEl.append(imageEl);
-// galleryEl.append(...itemImageEl);
+const itemImageEl = images.map((image) =>
+    `<li class="item">
+      <img class="pictures" url="${image.url}" alt="${image.alt}" width="360" height="300"/>
+    </li>`
+)
+.join('');
 
-// const list = document.querySelector('.gallery');
-// images.forEach(function (image) {
-//   const listItem = document.createElement('li');
-//   listItem.className = 'list';
-//   const imageItem = document.createElement('img');
-//   imageItem.src = image.url;
-//   imageItem.alt = image.alt;
-//   imageItem.className = 'photo';
-//   listItem.appendChild(imageItem);
-//   list.appendChild(listItem);
-// });
+galleryEl.insertAdjacentHTML("beforeend", itemImageEl)
+
